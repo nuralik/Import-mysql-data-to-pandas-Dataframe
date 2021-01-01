7b5f631e68686f2f2c1d3f471284af4d30344117
Nueraili Kuerbanjiang <30909427+nuralik@users.noreply.github.com>
2021-01-01 02:50:54 +0000

    Update data.py

--- data.py
+++ data.py
@@ -9,19 +9,14 @@ class mysql_data:
         self.Start_time = Start_time
         self.End_time = End_time
 
-    def rev4(device_id,Start_time,End_time):
-         db_connection=mysql.connector.connect(user='cuairlab',password='cuairlab@camp292',host='128.153.15.208',database='ESP32',use_pure=True)
+    def grabData(device_id,Start_time,End_time):
+         db_connection=mysql.connector.connect(user='USER_NAME',password='PASS_WORD',host='IP',database='DATA_BASE',use_pure=True)
          db_cursor=db_connection.cursor()
          db_cursor.execute("SELECT * FROM {}  WHERE reg_date BETWEEN '{}' and '{}'".format(device_id,Start_time, End_time))
          df=pd.DataFrame(db_cursor.fetchall())
          return df
     
-    def argon(device_id,Start_time,End_time):
-        db_connection=mysql.connector.connect(user='cuairlab',password='cuairlab@camp292',host='128.153.15.208',database='Particle',use_pure=True)
-        db_cursor=db_connection.cursor()
-        db_cursor.execute("SELECT * FROM {}  WHERE reg_date BETWEEN '{}' and '{}'".format(device_id,Start_time, End_time))
-        df=pd.DataFrame(db_cursor.fetchall())
-        return df
+    
     
 
      
